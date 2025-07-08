5dc99bf7a3b3ed5380b9c0ceb9f20d7e0461899e
Rafael Xavier <rafaellx10@gmail.com>
2025-07-07 21:21:01 -0300

    Refactor MenuItens widget to simplify layout structure and enhance readability

--- lib/pages/actions/menu_itens.dart
+++ lib/pages/actions/menu_itens.dart
@@ -16,41 +16,33 @@ class _MenuItensState extends State<MenuItens> {
       margin: const EdgeInsets.symmetric(vertical: 10.0),
       child: SingleChildScrollView(
         scrollDirection: Axis.horizontal,
-        child: Wrap(
-          direction: Axis.horizontal,
-          children: [
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 7),
-              child: Column(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 10.0),
-                    padding: const EdgeInsets.all(10.0),
-                    decoration: BoxDecoration(
-                      color: greyColor,
-                      borderRadius: BorderRadius.circular(50.0),
-                    ),
-                    child: IconButton(
-                      icon: Icon(
-                        MdiIcons.creditCard,
-                        color: Colors.black,
-                        size: 30.0,
-                      ),
-                      onPressed: () => print('onPressed: Transferir'),
-                    ),
-                  ),
-                  Text(
-                    'Transferir',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
+        child: Wrap(direction: Axis.horizontal, children: [_itemMenu()]),
+      ),
+    );
+  }
+
+  _itemMenu() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 7),
+      child: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(bottom: 10.0),
+            padding: const EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+              color: greyColor,
+              borderRadius: BorderRadius.circular(50.0),
             ),
-          ],
-        ),
+            child: IconButton(
+              icon: Icon(MdiIcons.creditCard, color: Colors.black, size: 30.0),
+              onPressed: () => print('onPressed: Transferir'),
+            ),
+          ),
+          Text(
+            'Transferir',
+            style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+          ),
+        ],
       ),
     );
   }
