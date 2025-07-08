809d0c97a0fb46f4e5a80f16ede2bf547ffa75cb
Rafael Xavier <rafaellx10@gmail.com>
2025-07-07 23:35:57 -0300

    Refactor Notifications widget to use _itemNotification method for improved readability and maintainability

--- lib/pages/home/notification/notification.dart
+++ lib/pages/home/notification/notification.dart
@@ -9,7 +9,23 @@ class Notifications extends StatelessWidget {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       child: Row(
-        children: [_income(), _income(), _income(), _income(), _income()],
+        children: [
+          _itemNotification(
+            'Seu ',
+            'Informe de rendimentos',
+            ' de 2025 já está disponível.',
+          ),
+          _itemNotification(
+            'Seu ',
+            'dividendo das ações',
+            ' já está disponível.',
+          ),
+          _itemNotification(
+            'Seu ',
+            'extrato de cartão',
+            ' já está disponível.',
+          ),
+        ],
       ),
     );
   }
@@ -43,6 +59,36 @@ class Notifications extends StatelessWidget {
               ),
             ),
             const TextSpan(text: ' de 2025 já está disponível.'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _itemNotification(String firstText, String highlightText, String lastText) {
+    return Container(
+      margin: const EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0),
+      padding: const EdgeInsets.all(24.0),
+      constraints: const BoxConstraints(maxWidth: 300.0),
+
+      decoration: BoxDecoration(
+        color: greyColor,
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+
+      child: RichText(
+        text: TextSpan(
+          style: const TextStyle(fontSize: 14.0, color: Colors.black),
+          children: [
+            TextSpan(text: firstText),
+            TextSpan(
+              text: highlightText,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.purple,
+              ),
+            ),
+            TextSpan(text: lastText),
           ],
         ),
       ),
